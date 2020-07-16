e8ec3b7843fe7480ebe2c9da3da3afc59fcc2958
ZhangJiChu <1339446165@qq.com>
2020-07-16 23:21:14 +0800

    Initial commit

---
+++ src/main/java/com/learning/HelloServlet.java
@@ -0,0 +1,23 @@
+package com.learning;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Author: ZHANG
+ * @Date: 2020/7/16
+ * @Description:
+ */
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("name", "小章");
+        req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req, resp);
+    }
+}
